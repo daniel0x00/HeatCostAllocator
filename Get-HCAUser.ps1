8bf3bee9d88ed0ed01a89bfa75519264543afddb
D.A. Ferreira <daniel@ferreira.fm>
2024-12-30 17:29:56 +0100

    User cmdlet finished.

--- Get-HCAUser.ps1
+++ Get-HCAUser.ps1
@@ -14,7 +14,31 @@ function Get-HCAUser {
 
     begin { }
     process {
-        
+        $RequestParameters = @{
+            Uri = $UserUrl
+            Method = 'Post'  
+            WebSession = $WebSession
+            Body = '{"JWT":"token","LANG":"en-US"}' -replace 'token', $JWTToken
+            ContentType = 'application/json'  
+            Headers = @{
+                "Accept" = "application/json"
+                "Sec-Fetch-Site" = "same-origin"
+                "Sec-Fetch-Mode" = "navigate"
+                "Sec-Fetch-Dest" = "document"
+                "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+            }
+        }
+
+        $Request = Invoke-RestMethod @RequestParameters -ErrorAction Stop -SkipCertificateCheck
+
+        # Output object:
+        $ReturnObject = New-Object System.Object
+        $ReturnObject | Add-Member -Type NoteProperty -Name WebSession -Value $WebSession
+        $ReturnObject | Add-Member -Type NoteProperty -Name JWTToken -Value $JWTToken
+        $ReturnObject | Add-Member -Type NoteProperty -Name User -Value $Request
+
+        $ReturnObject
+
     }
     end { }
 }
